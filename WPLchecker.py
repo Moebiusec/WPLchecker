41c56c60e1cba163a3422377749027a045499c19
Juampa Rodríguez <32236059+und3sc0n0c1d0@users.noreply.github.com>
2018-07-30 06:42:28 -0400

    Update WPLchecker.py

--- WPLchecker.py
+++ WPLchecker.py
@@ -13,7 +13,7 @@ __ |/ |/ / _  ____/_  /___/ /__ _  / / /  __/ /__ _  ,<  /  __/  /
 ____/|__/  /_/     /_____/\___/ /_/ /_/\___/\___/ /_/|_| \___//_/
 
 + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
-+                      [   WPLchecker v1.0   ]                      +
++                     [   WPLchecker  v1.0b   ]                     +
 +          [   64 Vulnerabilidades   ]  [   57 Plugins   ]          +
 +           Escrito por:  Juampa @UnD3sc0n0c1d0 Rodríguez           +
 +                 MOEBIUS TEAM - blog.moebiusec.com                 +
